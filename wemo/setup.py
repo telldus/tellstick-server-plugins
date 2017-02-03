c276218d810157b49611cf71e4db053d1d0d3cd2
Micke Prag <micke.prag@telldus.se>
2017-02-03 09:03:43 +0100

    Add metadata to wemo plugin

--- wemo/setup.py
+++ wemo/setup.py
@@ -8,11 +8,16 @@ except ImportError:
 
 setup(
 	name='WeMo',
-	version='0.1',
+	description='Control WEMO Insight',
+	version='1.0',
+	icon='icon.png',
+	color='#94e05c',
+	author='Micke Prag',
+	author_email='micke.prag@telldus.se',
 	packages=['wemo'],
 	package_dir = {'':'src'},
 	entry_points={ \
 		'telldus.startup': ['c = wemo:WeMo [cREQ]']
 	},
-	extras_require = dict(cREQ = 'Base>=0.1\nTelldus>=0.1\npywemo'),
+	extras_require = dict(cREQ = 'Base>=0.1\nTelldus>=0.1'),
 )
