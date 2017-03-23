d3429f0747e1faf50d178dcf8ae18273623fd790
Micke Prag <micke.prag@telldus.se>
2017-03-23 09:42:20 +0100

    Update Eliq plugin to latest plugin format

--- eliq/setup.py
+++ eliq/setup.py
@@ -8,14 +8,16 @@ except ImportError:
 
 setup(
 	name='Eliq',
-	version='0.1',
+	description='Import Eliq energy data into Telldus Live!',
+	version='1.0',
+	icon='eliq_logo.png',
+	color='#51b607',
+	author='Micke Prag',
+	author_email='micke.prag@telldus.se',
 	packages=['eliq'],
 	package_dir = {'':'src'},
 	entry_points={ \
 		'telldus.startup': ['c = eliq:Eliq [cREQ]']
 	},
-	extras_require = dict(cREQ = 'Base>=0.1\nTelldus>=0.1\nTelldusWeb>=0.1'),
-	package_data={'eliq' : [
-		'templates/*.html',
-	]}
+	extras_require = dict(cREQ = 'Base>=0.1\nTelldus>=0.1'),
 )
