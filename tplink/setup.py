f6c71fe263e3146d9cc1d0eebd30f6f04e7f949a
Fredrik Gullberg <fredrik.gullberg@telldus.se>
2017-03-30 10:53:30 +0200

    Add TPLink plugin

---
+++ tplink/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+	from setuptools import setup
+except ImportError:
+	from distutils.core import setup
+
+setup(
+	name='TP-Link',
+	version='1.0',
+	author='Fredrik Gullberg',
+	author_email='fredrik.gullberg@telldus.se',
+	color='#2ecfda',
+	description='Plugin that allows you to control TP-Link HS100/HS110',
+	icon='tplink.png',
+	long_description="",
+	packages=['tplink'],
+	entry_points={ \
+		'telldus.startup': ['c = tplink:TPLink [cREQ]']
+	},
+	extras_require = dict(cREQ = 'Base>=0.1\nTelldus>=0.1'),
+)
---
+++ tplink/tplink/TPLink.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+from base import Plugin
+from telldus import DeviceManager, Device
+from pyHS100 import TPLinkSmartHomeProtocol, SmartPlug
+from threading import Thread
+import logging
+
+class TPLinkDevice(Device):
+
+	def __init__(self, plug):
+		super(TPLinkDevice,self).__init__()
+		self.plug = plug
+		self.setName(plug.get_sysinfo()['alias'])
+
+	def _command(self, action, value, success, failure, **kwargs):
+		if action == Device.TURNON:
+			try:
+				self.plug.turn_on()
+			except:
+				failure(Device.FAILED_STATUS_TIMEDOUT)
+		elif action == Device.TURNOFF:
+			try:
+				self.plug.turn_off()
+			except:
+				failure(Device.FAILED_STATUS_TIMEDOUT)
+		else:
+			failure(0)
+			return
+		success()
+
+	def localId(self):
+		sysinfo = self.plug.get_sysinfo()
+		return sysinfo['deviceId']
+
+	def typeString(self):
+		return 'tplink'
+
+	def methods(self):
+		return Device.TURNON | Device.TURNOFF
+
+class TPLink(Plugin):
+	def __init__(self):
+		t = Thread(target=self.loadDevices, name='TP-Link device discovery')
+		t.start()
+
+	def loadDevices(self):
+		deviceManager = DeviceManager(self.context)
+		for dev in TPLinkSmartHomeProtocol.discover():
+			plug = SmartPlug(dev['ip'])
+			deviceManager.addDevice(TPLinkDevice(plug))
+		deviceManager.finishedLoading('tplink')
+	
