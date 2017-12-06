dee1918e04cb4234ef7954c14965306614f2a34e
Micke Prag <micke.prag@telldus.se>
2017-12-06 14:35:10 +0100

    Bump version

--- hue/setup.py
+++ hue/setup.py
@@ -20,7 +20,7 @@ class buildweb(bdist_egg):
 
 setup(
 	name='Philips Hue',
-	version='1.0',
+	version='1.1',
 	author='Telldus Technologies AB',
 	author_email='info.tech@telldus.se',
 	category='appliances',
