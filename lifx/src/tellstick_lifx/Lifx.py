88dab8e7b12daf26daee93fd2f78cc4a4d96a727
Micke Prag <micke.prag@telldus.se>
2018-01-15 15:44:40 +0100

    Pylint fixes

--- lifx/src/tellstick_lifx/Lifx.py
+++ lifx/src/tellstick_lifx/Lifx.py
@@ -1,18 +1,19 @@
 # -*- coding: utf-8 -*-
 
+import colorsys
+from threading import Timer
+
 from base import Plugin, mainthread
 from telldus import DeviceManager, Device
-from threading import Timer
 import lifx
-import colorsys
 
 class LifxDevice(Device):
 	def __init__(self, light):
-		super(LifxDevice,self).__init__()
+		super(LifxDevice, self).__init__()
 		self.light = light
 		self.setName(light.label)
 
-	def _command(self, action, value, success, failure, **kwargs):
+	def _command(self, action, value, success, failure, **__kwargs):
 		if action == Device.TURNON:
 			self.light.brightness = 1
 			self.light.power = True
@@ -22,11 +23,15 @@ class LifxDevice(Device):
 			self.light.brightness = value/255.0
 			self.light.power = True
 		elif action == Device.RGBW:
-			r = (value >> 24) & 0xFF
-			g = (value >> 16) & 0xFF
-			b = (value >> 8) & 0xFF
-			(h, s, l) = colorsys.rgb_to_hsv(r/255.0, g/255.0, b/255.0)
-			self.light.color = lifx.color.modify_color(self.light.color, hue=h*360.0, saturation=s)
+			red = (value >> 16) & 0xFF
+			green = (value >> 8) & 0xFF
+			blue = value & 0xFF
+			(hue, saturation, __l) = colorsys.rgb_to_hsv(red/255.0, green/255.0, blue/255.0)
+			self.light.color = lifx.color.modify_color(
+				self.light.color,
+				hue=hue*360.0,
+				saturation=saturation
+			)
 			self.light.power = True
 		else:
 			failure(0)
@@ -36,24 +41,26 @@ class LifxDevice(Device):
 	def localId(self):
 		return self.light.id
 
-	def typeString(self):
+	@staticmethod
+	def typeString():
 		return 'lifx'
 
-	def methods(self):
+	@staticmethod
+	def methods():
 		return Device.TURNON | Device.TURNOFF | Device.DIM | Device.RGBW
 
-class Lifx(Plugin):
+class Lifx(Plugin):  # pylint: disable=R0903
 	def __init__(self):
-		self.deviceManager = DeviceManager(self.context)
 		self.lifxClient = lifx.Client()
 		Timer(2.0, self.discover).start()
 
 	@mainthread
 	def discover(self):
+		deviceManager = DeviceManager(self.context)
 		for light in self.lifxClient.get_devices():
 			try:
-				d = LifxDevice(light)
+				device = LifxDevice(light)
 			except Exception:
 				continue
-			self.deviceManager.addDevice(d)
-		self.deviceManager.finishedLoading('lifx')
+			deviceManager.addDevice(device)
+		deviceManager.finishedLoading('lifx')
