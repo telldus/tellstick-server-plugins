5cdae736609b43e0f67f178cf9e7f95b32bec5cd
Micke Prag <micke.prag@telldus.se>
2018-01-15 15:53:41 +0100

    Bump version

--- lifx/setup.py
+++ lifx/setup.py
@@ -9,16 +9,16 @@ except ImportError:
 setup(
 	name='Lifx',
 	description='Plugin that allows you to control Lifx bulbs',
-	version='1.0',
+	version='1.1',
 	icon='icon.png',
 	color='#440099',
 	author='Micke Prag',
 	author_email='micke.prag@telldus.se',
 	category='appliances',
 	packages=['tellstick_lifx'],
-	package_dir = {'':'src'},
+	package_dir={'':'src'},
 	entry_points={ \
 		'telldus.startup': ['c = tellstick_lifx:Lifx [cREQ]']
 	},
-	extras_require = dict(cREQ = 'Base>=0.1\nTelldus>=0.1'),
+	extras_require=dict(cREQ='Base>=0.1\nTelldus>=0.1'),
 )
