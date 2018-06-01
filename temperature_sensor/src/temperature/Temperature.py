32c4963fa8129139209e753fbec51e1eed93b8f5
Micke Prag <micke.prag@telldus.se>
2018-06-01 13:32:26 +0200

    Sensors does not have commands

--- temperature_sensor/src/temperature/Temperature.py
+++ temperature_sensor/src/temperature/Temperature.py
@@ -11,28 +11,11 @@ class TemperatureSensor(Sensor):
 	'''All sensors exported must subclass Sensor
 
 	Minimal function to reimplement is:
-	_command
 	localId
 	typeString
 	'''
 	def __init__(self):
 		super(TemperatureSensor, self).__init__()
-
-	@staticmethod
-	def _command(action, value, success, failure, **__kwargs):
-		'''This method is called when someone want to control this sensor
-
-		action is the method id to execute. This could be for instance:
-		Sensor.TURNON or Sensor.TURNOFF
-
-		value us only used for some actions, for example dim
-
-		This method _must_ call either success or failure
-		'''
-		del value
-		del failure
-		logging.debug('Sending command %s to temperature sensor', action)
-		success()
 
 	@staticmethod
 	def localId():
