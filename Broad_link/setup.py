25ea9e406c1f20082e2e915c7c607fa497bba7a2
ajajul <ajajul9998555036@gmail.com>
2018-06-21 13:30:15 +0530

    improve control broadlink-switch
    change name,author,author_email and long_description in setup.py
    In Broadlink.py remove all template comments, assign mac address as local unique id, create thread for broadlink.discover() method, and remove code of if no devices was found then still creating device

--- Broad_link/setup.py
+++ Broad_link/setup.py
@@ -7,14 +7,14 @@ except ImportError:
 	from distutils.core import setup
 
 setup(
-	name='Broadlink device',
+	name='Broadlink',
 	version='1.0',
-	author='Alice',
-	author_email='alice@wonderland.lit',
+	author='Telldus Technologies',
+	author_email='info.tech@telldus.se',
 	color='#2c3e50',
 	description='Broadlink device for On/Off switched',
 	long_description="""
-		This plugin is used as a template when creating plugins that support new device types.
+		Plugin for controlling broadlink devices
 	""",
 	packages=['broadlinkdevice'],
 	package_dir = {'':'src'},
--- Broad_link/src/broadlinkdevice/Broadlink.py
+++ Broad_link/src/broadlinkdevice/Broadlink.py
@@ -1,77 +1,51 @@
 # -*- coding: utf-8 -*-
 
+import logging
+import codecs
+import broadlink
+
 from base import Plugin
 from telldus import DeviceManager, Device
-import logging
-import broadlink
-import codecs
+from threading import Thread
 
 class BroadDevice(Device):
-	'''All devices exported must subclass Device
-
-	Minimal function to reimplement is:
-	_command
-	localId
-	typeString
-	methods
-	'''
-	def __init__(self, device=None):
-		super(BroadDevice,self).__init__()
+	def __init__(self, uniqueId, device):
+		super(BroadDevice, self).__init__()
 		self.device = device
-		if self.device is not None:
-			self.device.auth()
+		self.device.auth()
+		self._uniqueId = uniqueId
 
 	def _command(self, action, value, success, failure, **kwargs):
-		'''This method is called when someone want to control this device
-
-		action is the method id to execute. This could be for instance:
-		Device.TURNON or Device.TURNOFF
-
-		value us only used for some actions, for example dim
-
-		This method _must_ call either success or failure
-		'''
 		logging.debug('Sending command %s to Broadlink device', action)
-		if self.device is not None:
-			if action == 1:
-				self.device.set_power(1)
-			else:
-				self.device.set_power(0)
+		if action == Device.TURNON:
+			self.device.set_power(1)
+		elif action == Device.TURNOFF:
+			self.device.set_power(0)
+		else:
+			failure(0)
+			return
 		success()
 
 	def localId(self):
-		'''Return a unique id number for this device. The id should not be
-		globally unique but only unique for this device type.
-		'''
-		return 8
+		return self._uniqueId
 
 	def typeString(self):
-		'''Return the device type. Only one plugin at a time may export devices using
-		the same typestring'''
-		return 'broad'
+		return 'broadlink'
 
 	def methods(self):
-		'''Return a bitset of methods this device supports'''
 		return Device.TURNON | Device.TURNOFF
 
 class Broadlink(Plugin):
-	'''This is the plugins main entry point and is a singleton
-	Manage and load the plugins here
-	'''
 	def __init__(self):
-		# The devicemanager is a globally manager handling all device types
 		self.deviceManager = DeviceManager(self.context)
+		self.deviceManager.finishedLoading('broadlink')
+		self.devices = None
+		t = Thread(target=self.detectBroadlink)
+		t.start()
 
-		# Load all devices this plugin handles here. Individual settings for the devices
-		# are handled by the devicemanager
-
+	def detectBroadlink(self):
 		self.devices = broadlink.discover(timeout=5)
 		for device in self.devices:
-			self.deviceManager.addDevice(BroadDevice(device))
-			
-		if not self.devices:
-			self.deviceManager.addDevice(BroadDevice())
-
-		# When all devices has been loaded we need to call finishedLoading() to tell
-		# the manager we are finished. This clears old devices and caches
-		self.deviceManager.finishedLoading('broad')
+			bytearray.reverse(device.mac)
+			uid = codecs.encode(device.mac, "hex_codec")
+			self.deviceManager.addDevice(BroadDevice(uid, device))
