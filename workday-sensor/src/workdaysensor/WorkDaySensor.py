ae52e5745e6f3448003f3f645e9ecf74d2e41f46
odavinash <odavinash786@gmail.com>
2018-08-16 00:07:17 +0530

    Remove extra comments from plugin

--- workday-sensor/src/workdaysensor/WorkDaySensor.py
+++ workday-sensor/src/workdaysensor/WorkDaySensor.py
@@ -12,43 +12,20 @@ import logging
 # pylint: disable=E0211,E0213,W0622,W0312
 
 class DummyDevice(Device):
-    	'''All devices exported must subclass Device
-
-	Minimal function to reimplement is:
-	_command
-	localId
-	typeString
-	methods
-	'''
 	def __init__(self):
 		super(DummyDevice,self).__init__()
 
 	def _command(self, action, value, success, failure, **kwargs):
-		'''This method is called when someone want to control this device
-
-		action is the method id to execute. This could be for instance:
-		Device.TURNON or Device.TURNOFF
-
-		value us only used for some actions, for example dim
-
-		This method _must_ call either success or failure
-		'''
 		logging.debug('Sending command %s to dummy device', action)
 		success()
 
 	def localId(self):
-		'''Return a unique id number for this device. The id should not be
-		globally unique but only unique for this device type.
-		'''
 		return 1
 
 	def typeString(self):
-		'''Return the device type. Only one plugin at a time may export devices using
-		the same typestring'''
 		return 'dummy'
 
 	def methods(self):
-		'''Return a bitset of methods this device supports'''
 		return Device.TURNON | Device.TURNOFF
 
 class WorkDaySensor(Plugin):
