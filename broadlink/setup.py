bee20ec07b04c597c135c6b77be630c3d5540083
Micke Prag <micke.prag@telldus.se>
2018-10-05 15:10:05 +0200

    Add metadata

--- broadlink/setup.py
+++ broadlink/setup.py
@@ -6,16 +6,20 @@ try:
 except ImportError:
 	from distutils.core import setup
 
+# The python library broadlink depends on pycryptodome which includes c extentions
+# It seems the api is equal to PyCrypto so just skipping this dependency seems to work for now
+
 setup(
-	name='Broadlink',
+	name='Broadlink-HA',
 	version='1.0',
 	author='Telldus Technologies',
 	author_email='info.tech@telldus.se',
-	color='#2c3e50',
-	description='Broadlink device for On/Off switched',
-	long_description="""
-		Plugin for controlling broadlink devices
-	""",
+	category='appliances',
+	color='#14b1ee',
+	icon='broadlink.png',
+	description='Broadlink device support',
+	long_description="""Plugin for controlling device from Broadlink devices
+These devices are also sold under other brands. Such as ClasOhlson.""",
 	packages=['broadlinkdevice'],
 	entry_points={ \
 		'telldus.startup': ['c = broadlinkdevice:Broadlink [cREQ]']
