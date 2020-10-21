968621fa66a30fda13df7aad73dc3523452ec52a
Micke Prag <micke.prag@telldus.se>
2020-10-21 14:09:29 +0200

    Lint fixes

--- upnp/src/upnp/SSDP.py
+++ upnp/src/upnp/SSDP.py
@@ -9,30 +9,34 @@ from base import ObserverCollection, IInterface, Application, Plugin, mainthread
 import logging
 
 class ISSDPNotifier(IInterface):
-	def ssdpRootDeviceFound(rootDevice):
+	def ssdpRootDeviceFound(rootDevice):  # pylint: disable=no-self-argument
 		"""This method is called when a root device is found on the network"""
-	def ssdpDeviceFound(device):
+
+	def ssdpDeviceFound(device):  # pylint: disable=no-self-argument
 		"""This method is called when a device is found on the network"""
-	def ssdpServiceFound(service):
+
+	def ssdpServiceFound(service):  # pylint: disable=no-self-argument
 		"""This method is called when a service is found on the network"""
+
 
 class SSDPResponse(object):
 	ST_ROOT_DEVICE, ST_DEVICE, ST_SERVICE, ST_UNKNOWN = range(4)
 
 	class _FakeSocket(StringIO.StringIO):
-		def makefile(self, *args, **kw):
+		def makefile(self, *_args, **_kwargs):
 			return self
+
 	def __init__(self, response):
-		r = httplib.HTTPResponse(self._FakeSocket(response))
-		r.begin()
-		self.location = r.getheader("location", '')
-		self.usn = r.getheader("usn", '')
-		self.st = r.getheader("st", '')
+		httpResponse = httplib.HTTPResponse(self._FakeSocket(response))
+		httpResponse.begin()
+		self.location = httpResponse.getheader("location", '')
+		self.usn = httpResponse.getheader("usn", '')
+		self.st = httpResponse.getheader("st", '')  # pylint: disable=invalid-name
 		try:
-			self.cache = r.getheader("cache-control").split("=")[1]
+			self.cache = httpResponse.getheader("cache-control").split("=")[1]
 		except:
 			logging.warning("Could not extract cache-control header.")
-			logging.warning("Headers are: %s", r.getheaders())
+			logging.warning("Headers are: %s", httpResponse.getheaders())
 		index = self.usn.find('::')
 		if index >= 0:
 			self.uuid = self.usn[5:index]
@@ -45,27 +49,31 @@ class SSDPResponse(object):
 			self.type = SSDPResponse.ST_DEVICE
 			self.deviceType = self.st[28:]
 
+
 class SSDP(Plugin):
 	observers = ObserverCollection(ISSDPNotifier)
 
 	def __init__(self):
 		self.rootDevices = {}
 		self.devices = {}
-		Application().registerScheduledTask(fn=self.startDiscover, minutes=10, runAtOnce=True)
+		Application().registerScheduledTask(
+		    fn=self.startDiscover, minutes=10, runAtOnce=True
+		)
 
 	def startDiscover(self):
-		t = Thread(target=self.discover, name='SSDP discoverer')
-		t.daemon = True
-		t.start()
+		thread = Thread(target=self.discover, name='SSDP discoverer')
+		thread.daemon = True
+		thread.start()
 
 	def discover(self):
 		service = "ssdp:all"
 		group = ("239.255.255.250", 1900)
-		message = "\r\n".join([
-			'M-SEARCH * HTTP/1.1',
-			'HOST: {0}:{1}',
-			'MAN: "ssdp:discover"',
-			'ST: {st}','MX: 3','',''])
+		message = "\r\n".join(
+		    [
+		        'M-SEARCH * HTTP/1.1', 'HOST: {0}:{1}', 'MAN: "ssdp:discover"',
+		        'ST: {st}', 'MX: 3', '', ''
+		    ]
+		)
 		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 		sock.settimeout(5)
 		sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
